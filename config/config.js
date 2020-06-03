0df8a9b199724cfe839bce02842e16f27daa43b4
cafajardop <carlosafp84@hotmail.com>
2020-06-02 23:40:45 -0500

    config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
 
-// Google
+// Google aut
 module.exports.GOOGLE_CLIENT_ID = '733235238457-ha943qd64eg45oooc10kjbfqucso8djg.apps.googleusercontent.com';
 module.exports.GOOGLE_SECRET = '8vwhraA3aKXcU5V4O3SwIFGW';
